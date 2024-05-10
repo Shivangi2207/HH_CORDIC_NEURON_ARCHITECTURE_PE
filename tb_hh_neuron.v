7ccd0ff0cd413ab25fc030ba8b5391789cd9544c
Shivangi <140998647+Shivangi2207@users.noreply.github.com>
2024-05-10 16:12:14 +0530

    Update tb_hh_neuron.v

--- tb_hh_neuron.v
+++ tb_hh_neuron.v
@@ -20,30 +20,58 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module tb(
+module tb();
 
-    );
     reg clk;
     reg reset;
-     hh_neuron  dut(
-     clk,
- reset
-);
+    reg [21:0] dt;
+    reg [21:0] C;
+    reg [21:0] g_Na;
+    reg [21:0] g_K;
+    reg [21:0] g_L;
+    reg [21:0] V_Na;
+    reg [21:0] V_K;
+    reg [21:0] V_L;
+    reg [21:0] e;
+    reg [21:0] a;
+    reg [21:0] I_inj;
 
-initial
-begin
-clk=0;
-reset=0;
- end
- 
- always #5 clk=~clk;
- 
- initial begin
- #5 reset=0;
- #5 reset=1;
- #10 reset=0;
- 
- end
- 
+    hh_neuron dut(
+        .clk(clk),
+        .reset(reset),
+        .dt(dt),
+        .C(C),
+        .g_Na(g_Na),
+        .g_K(g_K),
+        .g_L(g_L),
+        .V_Na(V_Na),
+        .V_K(V_K),
+        .V_L(V_L),
+        .e(e),
+        .a(a),
+        .I_inj(I_inj)
+    );
+
+    initial begin
+        clk = 0;
+        reset = 0;
+        dt = 22'd40;
+        C = 22'd40;
+//        g_Na = 22'd491;
+//        g_K = 22'd147;
+//        g_L = 22'd1;
+//        V_Na = 22'd225;
+//        V_K = -22'd295;
+//        V_L = -22'd202;
+        e = 22'd27182818; // 2.7182818 in fixed point format
+        a = 22'd1; // 0.1 in fixed point format
+        I_inj = 22'd4;
+        
+        #5 reset = 1;
+        #10 reset = 0;
+        #100 $finish;
+    end
+
+    always #5 clk = ~clk;
 
 endmodule
